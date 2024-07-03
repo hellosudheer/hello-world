de796a12afc119a75c261a8aafeb34f44b6b158a
Sudheer S <75902233+hellosudheer@users.noreply.github.com>
2024-07-03 14:16:51 -0400

    Update Tutorial1.py

--- Tutorial1.py
+++ Tutorial1.py
@@ -1,1 +1,12 @@
 print ("Hello World")
+
+num1 = 10
+num2 = 11
+
+#adding the number
+sum = num1+ num2
+
+#printing the sum
+print("The sum is ", sum)
+
+print("The sum of ", num1, " and " ,num2, " is sum")
