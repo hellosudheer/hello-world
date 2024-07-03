fb98588e1d4b7adb740499b3624e21e34cb3e641
Sudheer S <75902233+hellosudheer@users.noreply.github.com>
2024-07-03 14:26:07 -0400

    Update Tutorial1.py

--- Tutorial1.py
+++ Tutorial1.py
@@ -3,10 +3,20 @@ print ("Hello World")
 num1 = 10
 num2 = 11
 
-#adding the number
+#Adding two numbers
 sum = num1+ num2
 
 #printing the sum
 print("The sum is ", sum)
 
 print("The sum of ", num1, " and " ,num2, " is sum")
+
+
+#Getting inputs and then calculating the sum
+
+num3 = int(input("Enter the number 3: "))
+num4 = int(input("Enter the number 4: "))
+
+sum2 = num3+num4
+
+print("The sum is ", sum2)
